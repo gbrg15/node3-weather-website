9a74faaf50cc980cf90486617146dd8960a6eb5e
Giovanni Braghieri <giovannibraghieri@Galaxy-A50.localdomain>
2022-03-17 18:37:44 +0100

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -9,8 +9,9 @@
     const location = search.value
     messageOne.textContent = 'Loading'
     messageTwo.textContent = ' '
-    fetch('http://localhost:3000/weather?address='+location).then((response) =>{ 
- response.json().then((data)=>{
+
+    fetch('/weather?address='+location).then((response) =>{ 
+        response.json().then((data)=>{
         if (data.error) {
             messageOne.textContent = 'Unable to find location'
             messageTwo.textContent = ' '
